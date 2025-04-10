b0ecf4e47bde764e7b344a7c4de3c77d2a892e0a
Rahul <rahul.gupta@5exceptions.com>
2025-04-10 17:25:38 +0530

    Expose  version, tags and metadata media event method ,search and getResultPath and getAllImagePathsInDirectoryPng method are removed

--- TruVideoMediaAndroidBinding/DataListener.cs
+++ TruVideoMediaAndroidBinding/DataListener.cs
@@ -4,6 +4,7 @@ public class DataListener : Java.Lang.Object, TruVideoMediaAndroid.IDataListener
 {
     // Event to forward data to .NET MAUI
     public event Action<string>? DataReceived;
+    
 
     public void OnDataReceived(string data)
     {
